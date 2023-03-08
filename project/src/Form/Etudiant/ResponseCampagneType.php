a6e79efbabd81e124e1c6dc4647da1d318408430
william varlet <william.varlet8020@gmail.com>
2023-03-08 01:22:40 +0100

    Fix oublie de retour en arriére sur certain fichiers

--- project/src/Form/Etudiant/ResponseCampagneType.php
+++ project/src/Form/Etudiant/ResponseCampagneType.php
@@ -4,6 +4,7 @@ namespace App\Form\Etudiant;
 
 use App\Entity\CampagneChoix;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,14 +14,11 @@ class ResponseCampagneType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-//            ->add('blocOptions', CollectionType::class, [
-//                'entry_type' => BlocOptionType::class,
-//                'entry_options' => ['label' => false],
-//                'allow_add' => false,
-//                'allow_delete' => false,
-//                'label' => false,
-//            ])
-            ->add('parcours', ParcoursType::class, [
+            ->add('blocOptions', CollectionType::class, [
+                'entry_type' => BlocOptionType::class,
+                'entry_options' => ['label' => false],
+                'allow_add' => false,
+                'allow_delete' => false,
                 'label' => false,
             ]);
     }
