f3ea23e0d7114cd0c9ec86ff9d3706de8b3bdd1e
LeeDarmon <leemickael.darmon@outlook.fr>
2023-04-02 19:52:26 +0200

    conversion de l'entité usergestion en etudiant(+ toutes les pages), + pagination des etudiants importés

---
+++ project/src/Controller/EtudiantController.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Etudiant;
+use App\Entity\User;
+use App\Form\EtudiantType;
+use App\Form\UserImportType;
+use App\Repository\EtudiantRepository;
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Service\MailerService;
+use Knp\Component\Pager\PaginatorInterface;
+
+#[Route('/admin/etudiant')]
+class EtudiantController extends AbstractController
+{
+    public function __construct(private readonly MailerService $mailerService)
+    {
+    }
+
+    #[Route('/', name: 'app_etudiant_index', methods: ['GET', 'POST'])]
+    public function index(Request $request,EtudiantRepository $etudiantRepository,UserRepository $userRepository,UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager,PaginatorInterface $paginator): Response
+    {
+        $form = $this->createForm(UserImportType::class);
+        $form->handleRequest($request);
+        
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $fileImport = $form->get('fileImport')->getData();
+
+            $fileImport = fopen($fileImport, 'r');
+
+            if($fileImport){
+                while(($data = fgetcsv($fileImport)) !== false) {
+                    if($data[0] != 'nom'){
+                        $etudiant = new Etudiant();
+                        $etudiant->setNom($data[0]);
+                        $etudiant->setPrenom($data[1]);
+                        $etudiant->setMail($data[2]);
+                        $etudiantRepository->save($etudiant, true);
+
+                        $user = new User();
+                        $user->setEmail($data[2]);
+                        $user->setPassword('default');
+                        $userRepository->save($user,true);
+                        // generate a signed url and email it to the user
+                        $this->mailerService->sendEmailConfirmation('app_verify_email', $user);
+                    }
+                }
+            }
+            return $this->redirectToRoute('app_etudiant_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+
+        $query = $etudiantRepository->findAll();
+
+        $etudiants = $paginator->paginate(
+            $query,
+            $request->query->getInt('page', 1),
+            10
+        );
+
+        return $this->render('etudiant/index.html.twig', [
+            'etudiants' => $etudiants,
+            'form'=> $form->createView()
+        ]);
+    }
+
+    #[Route('/new', name: 'app_etudiant_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EtudiantRepository $etudiantRepository): Response
+    {
+        $etudiant = new Etudiant();
+        $form = $this->createForm(EtudiantType::class, $etudiant);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $etudiantRepository->save($etudiant, true);
+
+            return $this->redirectToRoute('app_etudiant_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('etudiant/new.html.twig', [
+            'etudiant' => $etudiant,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_etudiant_show', methods: ['GET'])]
+    public function show(Etudiant $etudiant): Response
+    {
+        return $this->render('etudiant/show.html.twig', [
+            'etudiant' => $etudiant,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_etudiant_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Etudiant $etudiant, EtudiantRepository $etudiantRepository): Response
+    {
+        $form = $this->createForm(EtudiantType::class, $etudiant);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $etudiantRepository->save($etudiant, true);
+
+            return $this->redirectToRoute('app_etudiant_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('etudiant/edit.html.twig', [
+            'etudiant' => $etudiant,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_etudiant_delete', methods: ['POST'])]
+    public function delete(Request $request, Etudiant $etudiant, EtudiantRepository $etudiantRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$etudiant->getId(), $request->request->get('_token'))) {
+            $etudiantRepository->remove($etudiant, true);
+        }
+
+        return $this->redirectToRoute('app_etudiant_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
--- project/src/Form/UserImportType.php
+++ project/src/Form/UserImportType.php
@@ -2,7 +2,7 @@
 
 namespace App\Form;
 
-use App\Entity\UserGestion;
+use App\Entity\Etudiant;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -38,7 +38,7 @@ class UserImportType extends AbstractType
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => UserGestion::class,
+            'data_class' => Etudiant::class,
         ]);
     }
 }
