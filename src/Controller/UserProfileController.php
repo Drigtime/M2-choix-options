53711c452d7974e95f37da2cd12cefa8df09bb09
Drigtime <drigtime@gmail.com>
2023-07-03 21:12:00 +0200

    Fix prev commit

--- src/Controller/UserProfileController.php
+++ src/Controller/UserProfileController.php
@@ -2,25 +2,32 @@
 
 namespace App\Controller;
 
+use App\Entity\User\User;
 use App\Form\UserEmailType;
 use App\Form\UserPasswordType;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserProfileController extends AbstractController
 {
     #[Route('/user/profile', name: 'app_user_profile')]
     #[IsGranted('ROLE_USER')]
-    public function index(Request $request, UserRepository $userRepository): Response
-    {
+    public function index(
+        Request $request,
+        UserPasswordHasherInterface $userPasswordHasher,
+        UserRepository $userRepository
+    ): Response {
         $user = $this->getUser();
-
-        // form to edit email and an other form to edit password
+        if (!$user) {
+            throw $this->createNotFoundException('Utilisateur non trouvé.');
+        }
+        /** @var User $user */
 
         $formEmail = $this->createForm(UserEmailType::class, $user);
         $formPassword = $this->createForm(UserPasswordType::class, $user);
@@ -29,12 +36,19 @@ class UserProfileController extends AbstractController
         $formPassword->handleRequest($request);
 
         if ($formEmail->isSubmitted() && $formEmail->isValid()) {
+            // upgradePassword(PasswordAuthenticatedUserInterface $user, string $newHashedPassword): void
             $userRepository->save($user, true);
 
             $this->addFlash('success', 'Votre email a bien été modifié.');
         }
 
         if ($formPassword->isSubmitted() && $formPassword->isValid()) {
+            $user->setPassword(
+                $userPasswordHasher->hashPassword(
+                    $user,
+                    $formPassword->get('password')->getData()
+                )
+            );
             $userRepository->save($user, true);
 
             $this->addFlash('success', 'Votre mot de passe a bien été modifié.');
@@ -45,6 +59,5 @@ class UserProfileController extends AbstractController
             'formEmail' => $formEmail->createView(),
             'formPassword' => $formPassword->createView(),
         ]);
-
     }
 }
